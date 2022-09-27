061c507e947ae583339250bccc90cd46a3d5bd22
Cristian Tirimacco <cristian.tirimacco.contractor@gdevtools.com>
2022-09-27 10:58:15 -0300

    Initial commit

---
+++ src/main/java/com/neoris/api/users/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.neoris.api.users.repository;
+
+import com.neoris.api.users.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+
+
+}
